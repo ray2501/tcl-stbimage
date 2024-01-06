f827120544140e2b39d52bf3de727498fecb725f
Danilo Chang <ray2501@gmail.com>
2024-01-06 13:19:21 +0800

    Bump version to v0.8

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded stbimage 0.7 \
-	    [list load [file join $dir libtcl9stbimage0.7.so] [string totitle stbimage]]
+    package ifneeded stbimage 0.8 \
+	    [list load [file join $dir libtcl9stbimage0.8.so] [string totitle stbimage]]
 } else {
-    package ifneeded stbimage 0.7 \
-	    [list load [file join $dir libstbimage0.7.so] [string totitle stbimage]]
+    package ifneeded stbimage 0.8 \
+	    [list load [file join $dir libstbimage0.8.so] [string totitle stbimage]]
 }
