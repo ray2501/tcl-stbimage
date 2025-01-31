7edd26b66810ed1fc34f27c279a03e395f7cd283
Danilo Chang <ray2501@gmail.com>
2025-01-31 20:11:30 +0800

    Bump version to v1.3

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded stbimage 1.2 \
-	    [list load [file join $dir libtcl9stbimage1.2.so] [string totitle stbimage]]
+    package ifneeded stbimage 1.3 \
+	    [list load [file join $dir libtcl9stbimage1.3.so] [string totitle stbimage]]
 } else {
-    package ifneeded stbimage 1.2 \
-	    [list load [file join $dir libstbimage1.2.so] [string totitle stbimage]]
+    package ifneeded stbimage 1.3 \
+	    [list load [file join $dir libstbimage1.3.so] [string totitle stbimage]]
 }
